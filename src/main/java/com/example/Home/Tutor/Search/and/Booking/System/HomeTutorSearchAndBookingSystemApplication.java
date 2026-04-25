9c8b8d6ed21daeeb335a0c39df2f57a524507ee0
ashiruchathun <ashiruchathun@gmail.com>
2026-04-25 13:13:19 +0530

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/example/Home/Tutor/Search/and/Booking/System/HomeTutorSearchAndBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Home.Tutor.Search.and.Booking.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeTutorSearchAndBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeTutorSearchAndBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Home/Tutor/Search/and/Booking/System/HomeTutorSearchAndBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Home.Tutor.Search.and.Booking.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeTutorSearchAndBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
